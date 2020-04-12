3f52570cd2ee5515b9dbaf9f45ad302f51db3367
pnuehrenberg <paul.nuehrenberg@uni.kn>
2020-04-12 16:00:32 +0200

    check surv analysis R

--- scripts/R/survivalanalysis.R
+++ scripts/R/survivalanalysis.R
@@ -2,12 +2,11 @@ library(survival)
 library(ggplot2)
 library(ggfortify)
 
-setwd('~/Documents/BurtoniLearningExp/learning/')
-learning = read.csv('LearningResults.csv')
+setwd('../../data/spreadsheets')
+learning = read.csv('learning.csv')
 
 # subset data, we only analyze dom and sub demonstrators
 learning = learning[learning$STATUS %in% c(0, 1, 2), ]
-learning$DEMONSTRATOR = as.factor(learning$DEMONSTRATOR) # has demonstrator: 1 - yes or 0 - no
 learning$STATUS = as.factor(learning$STATUS) # type of demonstrator: 0 - None, 1 - dom, 2 - sub
 
 ##################################################################################
@@ -19,14 +18,8 @@ autoplot(km_fit)
 cox = coxph(Surv(TRIAL, SURVIVAL) ~ STATUS, data=learning)
 summary(cox)
 
-# switch dom and None factors to see all effects
-
-learning = read.csv('LearningResults.csv')
-learning = learning[learning$STATUS %in% c(0, 1, 2), ]
-learning$STATUS[learning$STATUS == 0] = 3
-learning$STATUS[learning$STATUS == 1] = 0
-learning$STATUS[learning$STATUS == 3] = 1
-learning$STATUS = as.factor(learning$STATUS)
+# relevel status to see all effects
+learning$STATUS = relevel(learning$STATUS, 2)
 
 km_fit <- survfit(Surv(TRIAL, SURVIVAL) ~ STATUS, data=learning)
 autoplot(km_fit)
