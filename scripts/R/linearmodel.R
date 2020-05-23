ec93a53526bd86f7a1d73ef46e88b5693ab3f8be
pnuehrenberg <paul.nuehrenberg@uni-konstanz.de>
2020-05-23 20:36:11 +0200

    noise frequncy means (also model had unnecessary logit transform we did not use)

--- scripts/R/linearmodel.R
+++ scripts/R/linearmodel.R
@@ -4,14 +4,16 @@ library(boot)
 
 setwd('../../data/spreadsheets')
 social_parameters = read.csv('social_parameters.csv')
+social_parameters$social_status = as.factor(social_parameters$social_status)
 social_parameters$social_status = relevel(social_parameters$social_status, 'SUB')
 
-boxplot(logit(noise_frequency) ~ social_status, data=social_parameters)
+boxplot(noise_frequency ~ social_status, data=social_parameters)
 
-model <- lm(logit(noise_frequency) ~ social_status, data=social_parameters)
+model <- lm(noise_frequency ~ social_status, data=social_parameters)
 plot(model)
 shapiro.test(model$residuals) # W = 0.96403, p-value = 0.07423
 bptest(model) # BP = 2.8357, df = 1, p-value = 0.09219 (BP is chi²)
 
 summary(model)
 
+aggregate(social_parameters$noise_frequency, FUN = mean, by = list(social_parameters$social_status))
